6b219942d19109d7f598a07bb815de0091c723f7
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-03-01 09:21:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+// Javascript is a high-level, prototype-based object-oriented, multi-paradigm, interpreted or just in time compiled, dynamic, single-threaded, garbage-collected programming language with first-class functions and a non-blocking event loop concurrency model.
+
+/*Multi-Paradigm 
+- Procedural Programming
+- Objected-Oriented Programming
+- Functional Programming.
+JS does all the above. Paradigm could also be imperative or declarative.
+Almost everything in JS is an Object except for primitive values (e.g strings, numbers)
+
+// FIRST-CLASS FUNCTIONS
+Functions are simply treated as variables. They can be passed into other functions, and return them from functions.
+
+// DYNAMIC
+JS is dynamically typed, you do not have to assign data type to variables, rather they become known during code execution.
+
+*/
